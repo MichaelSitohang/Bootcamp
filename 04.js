bfd15907e33e870950d0d6e8721a0e7be283022c
MichaelSitohang <michaelsitohang2030@gmail.com>
2024-09-13 09:30:10 +0700

    pembelajaran hari ke empat

--- 04.js
+++ 04.js
@@ -2,47 +2,45 @@ const validator = require("validator");
 const fs = require("fs");
 const readline = require("node:readline");
 const { stdin: input, stdout: output } = require("node:process");
-const { fileURLToPath } = require("url");
-const { stringify } = require("querystring");
-
 const rl = readline.createInterface({ input, output });
 
-rl.question("Siapa nama kamu? ", (nama) => {
-  rl.question("Berapa nomor telepon kamu?", (mobile) => {
-    rl.question("apa nama email kamu?", (email) => {
-      const newcontact = addData(nama, mobile, email);
-      if (newcontact) {
-        Savedata(newcontact);
-      }
-      rl.close();
+// Fungsi untuk menambahkan data
+function addData() {
+  rl.question("Siapa nama kamu ? ", (nama) => {
+    rl.question("berapa nomor hp kamu?", (nomorhp) => {
+      rl.question("apa alamat email kamu?", (email) => {
+        if (!validator.isMobilePhone(nomorhp)) {
+          console.log("Format nomor telepon anda salah");
+          rl.close();
+          return;
+        }
+        if (!validator.isEmail(email)) {
+          console.log("Format Email anda salah");
+          rl.close();
+          return;
+        }
+
+        const result = { nama, nomorhp, email };
+        saveData(result);
+        rl.close();
+      });
     });
   });
-});
-
-function addData(nama, mobile, email) {
-  if (!validator.isMobilePhone(mobile)) {
-    console.log("Format nomor telepon anda salah");
-    rl.close();
-    return;
-  }
-  if (!validator.isEmail(email)) {
-    console.log("Format email anda salah");
-    rl.close();
-    return;
-  }
 }
-function Savedata(result) {
+
+// Fungsi untuk menyimpan data ke file
+function saveData(result) {
   try {
-    const result = { nama, mobile, email };
-    const file = fs.readFileSync("data/contacts4.json", "utf-8");
+    const file = fs.readFileSync("data/contacts.json", "utf-8");
     const contact = JSON.parse(file);
     contact.push(result);
 
-    fs.writeFileSync("data/contacts4.json", JSON.stringify(contact));
-    rl.close();
-
-    console.log("Data telah berhasil disimpan ke contacts4.json");
-  } catch (error) {
-    console.log("Terjadi kesalahan", error.message);
+    fs.writeFileSync("data/contacts.json", JSON.stringify(contact));
+    console.log("Data telah berhasil disimpan ke jawaban.json");
+  } catch (err) {
+    console.error("Gagal menyimpan data", err);
   }
 }
+
+// Memulai proses input data
+addData();
