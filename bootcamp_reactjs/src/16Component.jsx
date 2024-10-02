9977146c00131e5ae4936d725dfbbc14733ed662
MichaelSitohang <michaelsitohang2030@gmail.com>
2024-10-02 14:57:45 +0700

    Hari ke 16 Belajar Reactjs_Jsx

---
+++ bootcamp_reactjs/src/16Component.jsx
@@ -0,0 +1,13 @@
+const Hello = () => {
+    const data = {
+         nama : "Michael Sitohang",
+         jabatan : "Penata Kelola Pemerintahan"
+    }
+
+    const data = n
+    return (
+        <div>
+        <h1></h1>
+
+        </div>)
+}
